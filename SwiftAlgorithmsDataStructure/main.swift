44acdea71f9caf701d28a746c23dcb5981daaa85
Aaron <tim2464@yahoo.com.tw>
2020-02-10 15:34:31 -0800

    Initial Commit

---
+++ SwiftAlgorithmsDataStructure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithmsDataStructure
+//
+//  Created by AaronH on 2020-02-10.
+//  Copyright © 2020 AaronH. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
