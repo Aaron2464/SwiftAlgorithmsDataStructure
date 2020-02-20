efc4a5d1dcf7d0465bea2e6d6a78b2b7ef9b31a5
Aaron <tim2464@yahoo.com.tw>
2020-02-20 14:28:54 -0800

    count the first board created

--- SwiftAlgorithmsDataStructure/miniProject2/QueensSolver.swift
+++ SwiftAlgorithmsDataStructure/miniProject2/QueensSolver.swift
@@ -14,17 +14,25 @@ import Foundation
 /// - Your method should stop exploring if it finds a solution
 /// - You are allowed to change the function header (args or return type)
 /// - Your total recursive calls should not exceed 120 times.
-
+var sum = 0
 var count = 0
 var col = 0
 func solveQueens(board: inout Board, row: Int){
+    if count == 0{
+        sum += 1
+    }
+    
+    if row > board.size{
+        return
+    }
+    
     if row == board.size{
         count += 1
-        print("\(count) possible ways")
         print(board)
+        print("\(count) possible ways, \(sum) calls")
         return
     }else{
-        for col in 0...board.size{
+        for col in 0..<board.size{
             if board.isSafe(row: row, col: col){
                 board.place(row: row, col: col)
                 solveQueens(board: &board, row: row + 1)
